88acc1ad07024ccd2010e9bd58a254c2787934f2
Harry Turnbull <hturnbull@live.co.uk>
2020-05-15 12:17:01 +0100

    derp

--- db/migrate/20200515111307_add_first_name_last_name_to_posts.rb
+++ db/migrate/20200515111307_add_first_name_last_name_to_posts.rb
@@ -1,4 +1,4 @@
-class AddFirstNameToPosts < ActiveRecord::Migration[5.1]
+class AddFirstNameLastNameToPosts < ActiveRecord::Migration[5.1]
   def change
     add_column :posts, :first_name, :string
     add_column :posts, :last_name, :string
