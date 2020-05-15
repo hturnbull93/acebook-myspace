34b65e85ec3a00d5ed4d2f11c6927c9624533bbb
Harry Turnbull <hturnbull@live.co.uk>
2020-05-15 11:20:46 +0100

    addewd user functionality

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,46 +1,49 @@
 class Api::V1::PostsController < ApplicationController
 
-  before_action :set_post, only:[:show, :update, :destroy]
-
-
-#  def new
-#    @post = Post.new
-#  end
-
-#  def edit
-#    @post = Post.find(params[:id])
-#  end
+  before_action :set_post, only: [:show, :update, :destroy]
 
   def create
-    @post = Post.create(post_params)
-    if @post.save
-      render json: @post, location: api_v1_post_url(@post)
-    else
-      render json: @post.errors, status: :unprocessable_entity
+    begin
+      @post = Post.create(
+        message: params[:message],
+        user_id: params[:user_id]
+      )
+      render json: {
+        post: @post, 
+        location: api_v1_post_url(@post),
+        status: :created
+      }
+    rescue
+      render json: {
+        status: 500
+      }
     end
   end
 
-  def update
-    @post = Post.find(params[:id])
-    if @post.update(post_params)
-      render json: @posts
-    else
-      render json: @post.errors, status: :unprocessable_entry
-    end
-  end
+  # def update
+  #   @post = Post.find(params[:id])
+  #   if @post.update(post_params)
+  #     render json: @posts
+  #   else
+  #     render json: @post.errors, status: :unprocessable_entry
+  #   end
+  # end
 
   def index
     @posts = Post.all
-    render json: @posts
+    render json: {
+      status: 200,
+      posts: @posts
+    }
   end
 
-  def show
-    render json: @post
-  end
+  # def show
+  #   render json: @post
+  # end
   
-  def destroy
-    @post.destroy
-  end
+  # def destroy
+  #   @post.destroy
+  # end
 
   private
 
@@ -49,6 +52,6 @@ class Api::V1::PostsController < ApplicationController
   end
 
   def post_params
-    params.permit(:message)
+    params.permit(:message, :user_id)
   end
 end
