f84d7c380b4c62573ae56ae70084105a55de2511
Harry Turnbull <hturnbull@live.co.uk>
2020-05-15 11:20:29 +0100

    tests for posts controller, users added

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,29 +1,53 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe PostsController, type: :controller do
-#   describe "GET /new " do
-#     xit "responds with 200" do
-#       get :new
-#       expect(response).to have_http_status(200)
-#     end
-#   end
+RSpec.describe Api::V1::PostsController, type: :controller do
 
-#   describe "POST /" do
-#     xit "responds with 200" do
-#       post :create, params: { post: { message: "Hello, world!" } }
-#       expect(response).to redirect_to(posts_url)
-#     end
+  before(:each) do
+    @user = User.create(
+    first_name: "Sam",
+    last_name: "Gangi",
+    email: "samgangi@lotr.com",
+    password: "theshire"
+    )
+  end
 
-#     xit "creates a post" do
-#       post :create, params: { post: { message: "Hello, world!" } }
-#       expect(Post.find_by(message: "Hello, world!")).to be
-#     end
-#   end
+  it 'POST /posts with a user id responds with json' do
+    post :create, params: {
+      user_id: @user.id,
+      message: 'Mr Frodo'
+    }
 
-#   describe "GET /" do
-#     xit "responds with 200" do
-#       get :index
-#       expect(response).to have_http_status(200)
-#     end
-#   end
-# end
+    json = JSON.parse(response.body)
+
+    expect(json['status']).to eq "created"
+    expect(json['post']['message']).to eq "Mr Frodo"
+    expect(json['post']['user_id']).to eq @user.id
+  end
+
+  it 'POST /posts without a user id responds with json with 500' do
+    post :create, params: {
+      user_id: "a",
+      message: 'Mr Frodo'
+    }
+
+    json = JSON.parse(response.body)
+
+    expect(json['status']).to eq 500
+  end
+
+  it 'GET /posts returns all posts' do
+    Post.create(message: 'Mr Frodo', user_id: @user.id)
+    Post.create(message: "I'm coming with you", user_id: @user.id)
+    
+    get :index
+
+    json = JSON.parse(response.body)
+
+    expect(json['status']).to eq 200
+    expect(json['posts'].length).to eq 2
+    expect(json['posts'].first['message']).to eq 'Mr Frodo'
+    expect(json['posts'].last['message']).to eq "I'm coming with you"
+    expect(json['posts'].first['user_id']).to eq @user.id
+    expect(json['posts'].last['user_id']).to eq @user.id
+  end
+end
