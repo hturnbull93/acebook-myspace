f56b8527bbd14555d55f343bb34aed316a86fe09
Harry Turnbull <hturnbull@live.co.uk>
2020-05-15 10:28:39 +0100

    formatted and rm duplicate session assignment

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,28 +1,25 @@
 class Api::V1::SessionsController < ApplicationController
-    def create
-       @user = User.find_by(email: params[:email])
-       if @user && @user.authenticate(params[:password])
-         session[:user_id] = @user.id 
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id 
+      render json: {
+        status: :created,
+        user: @user,
+        logged_in: true
+      }
+    else
+      render json: {
+        status: 401
+      }
+    end 
+  end
 
-         session[:user_id] = @user.id
-          render json: {
-            status: :created,
-            user: @user,
-            logged_in: true
-          }
-       else
-        render json: {
-            status: 401
-          }
-       end 
-    end
-
-    def destroy 
-        session.clear
-        render json: {
-            status: 200,
-            logged_in: false
-        }
-    end
-
+  def destroy 
+    session.clear
+    render json: {
+      status: 200,
+      logged_in: false
+    }
+  end
 end
