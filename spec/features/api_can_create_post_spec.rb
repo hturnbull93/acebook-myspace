f03d5377c89326809f5f7bda9c044a4f7658a0b4
Harry Turnbull <hturnbull@live.co.uk>
2020-05-15 12:26:15 +0100

    fixed other tests with new user id requirement

--- spec/features/api_can_create_post_spec.rb
+++ spec/features/api_can_create_post_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 
 RSpec.feature "Posts API", type: :feature do
   scenario "Visiting /api/v1/posts?message=Hello&slug=HelloWorldreturns all posts" do
+    @user = User.create(
+      first_name: "Sam",
+      last_name: "Gangi",
+      email: "samgangi@lotr.com",
+      password: "theshire"
+    )
 
-    page.driver.post('/api/v1/posts?message=Hello') 
+    page.driver.post("/api/v1/posts?message=Hello&user_id=#{@user.id}") 
     expect(page).to have_content('"message":"Hello"')
   end
 end
--- spec/features/api_can_serve_all_posts_spec.rb
+++ spec/features/api_can_serve_all_posts_spec.rb
@@ -4,10 +4,16 @@ require 'rails_helper'
 
 RSpec.feature "Posts API", type: :feature do
   scenario "Visiting /api/v1/posts returns all posts" do
+    @user = User.create(
+      first_name: "Sam",
+      last_name: "Gangi",
+      email: "samgangi@lotr.com",
+      password: "theshire"
+    )
 
-    Post.create(message:"Hello!", slug:"HelloWorld")
+    Post.create(message:"Hello!", slug:"HelloWorld", user_id: @user.id)
     visit '/api/v1/posts'
-    # expect(page).to have_content('"id": 1')
+
     expect(page).to have_content('"message":"Hello!"')
     expect(page).to have_content('"slug":"HelloWorld"')
   end
